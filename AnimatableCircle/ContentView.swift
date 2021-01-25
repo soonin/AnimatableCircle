18ac3e46b75f7cd673abbdfd23d5989e952e3a0b
pooya <info@centurytrail.com>
2021-01-24 18:13:26 -0800

    Initial Commit

---
+++ AnimatableCircle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimatableCircle
+//
+//  Created by Pooya on 2021-01-24.
+//  Copyright © 2021 Pooya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
